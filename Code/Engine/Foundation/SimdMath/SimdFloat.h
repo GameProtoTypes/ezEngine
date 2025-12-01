4358663515a2d4ebd43ef93f47127d3eceb79da7
TrevorCash <cashtrevor@msn.com>
2025-11-30 19:57:49 -0700

    remove AVXFloat_inl.h because it is defined in SSE

--- Code/Engine/Foundation/SimdMath/SimdFloat.h
+++ Code/Engine/Foundation/SimdMath/SimdFloat.h
@@ -82,7 +82,7 @@ public:
 #if EZ_SIMD_IMPLEMENTATION == EZ_SIMD_IMPLEMENTATION_SSE
 #  include <Foundation/SimdMath/Implementation/SSE/SSEFloat_inl.h>
 #elif EZ_SIMD_IMPLEMENTATION == EZ_SIMD_IMPLEMENTATION_AVX
-#  include <Foundation/SimdMath/Implementation/AVX/AVXFloat_inl.h>
+#  include <Foundation/SimdMath/Implementation/SSE/SSEFloat_inl.h>
 #elif EZ_SIMD_IMPLEMENTATION == EZ_SIMD_IMPLEMENTATION_FPU
 #  include <Foundation/SimdMath/Implementation/FPU/FPUFloat_inl.h>
 #elif EZ_SIMD_IMPLEMENTATION == EZ_SIMD_IMPLEMENTATION_NEON
